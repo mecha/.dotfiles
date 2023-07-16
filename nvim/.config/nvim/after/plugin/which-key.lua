2e93d2fc891dd3d94c163881e3acc0d090a78231
Miguel Muscat <miguelmuscat93@gmail.com>
2023-07-16 12:34:24 +0200

    change: reorder which-key groups

--- nvim/.config/nvim/after/plugin/which-key.lua
+++ nvim/.config/nvim/after/plugin/which-key.lua
@@ -3,15 +3,15 @@ local wk = require("which-key")
 wk.setup()
 
 wk.register({
-    c = { name = "+Code" },
-    cl = { name = "+LSP" },
-    g = { name = "+Goto/Git" },
-    w = { name = "+Windows" },
-    b = { name = "+Buffers" },
-    n = { name = "+Notifications" },
-    h = { name = "+Harpoon" },
-    s = { name = "+Search" },
-    u = { name = "+UI" },
-    q = { name = "+Quit" },
-    x = { name = "+Diagnostics" },
+    g = { name = "Goto/Git" },
+    s = { name = "Search" },
+    c = { name = "Code" },
+    cl = { name = "LSP" },
+    cs = { name = "Swap" },
+    h = { name = "Harpoon" },
+    u = { name = "UI" },
+    w = { name = "Windows" },
+    b = { name = "Buffers" },
+    x = { name = "Diagnostics" },
+    q = { name = "Quit" },
 }, { prefix = "<leader>" })
