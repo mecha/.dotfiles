a689e8eac2cd99e2263bb6dfb464fbd774d5e879
Miguel Muscat <miguelmuscat93@gmail.com>
2023-08-25 20:40:13 +0200

    refactor(nvim): no middle slash in find/replace

--- nvim/.config/nvim/lua/mecha/keymaps.lua
+++ nvim/.config/nvim/lua/mecha/keymaps.lua
@@ -14,7 +14,7 @@ vim.keymap.set("n", "<C-s>", vim.cmd.w, { desc = "Save buffer", silent = true })
 -------------------------------------------------------------------------------
 
 vim.keymap.set("n", "<S-L>", "mzi<CR><ESC>`z", { desc = "Split line", silent = true })
-vim.keymap.set("n", "<C-h>", ":%s///g<left><left><left>", { desc = "Find/Replace in file" })
+vim.keymap.set("n", "<C-h>", ":%s//g<left><left>", { desc = "Find/Replace in file" })
 
 -------------------------------------------------------------------------------
 -- Move text
