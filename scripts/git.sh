334756c36092f024dcdfffd54e06d848be350ff8
Miguel Muscat <mail@miguelmuscat.me>
2026-03-06 11:33:13 +0100

    scripts: move git credentials to env vars

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,7 +1,17 @@
 #! /bin/env bash
 
-git config --global user.name "Miguel Muscat"
-git config --global user.email "mail@miguelmuscat.me"
+if [ -z "$GIT_NAME" ] || [ -z "$GIT_EMAIL" ]; then
+    echo "The \$GIT_NAME or \$GIT_EMAIL environment variables are not set."
+    echo
+    echo "GIT_NAME = $GIT_NAME"
+    echo "GIT_EMAIL = $GIT_EMAIL"
+    echo
+    echo "Add these vars to ~/.env and re-run the script."
+    exit 1
+fi
+
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
 git config --global core.autocrlf
 git config --global pull.rebase false
 git config --global remote.origin.receivepack "git receive-pack"
